9aac2d2c2e6ed600c313ab2aea5f066e0a394168
Velda Kiara <veldakarimi@gmail.com>
2020-05-22 13:13:09 +0300

    adding views

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
