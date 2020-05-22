942f626d090b369c8705cfc7fe0eb54e001b8b33
Velda Kiara <veldakarimi@gmail.com>
2020-05-22 13:12:14 +0300

    changes

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -25,11 +25,20 @@ def update(self, instance, validated_data):
     instance.style = validated_data.get('style', instance.style)
     instance.save()
     return instance
+
 '''create() and update() methods define how fully fledged 
     instances are created or modified when calling serializer.save()
     A serializer class is very similar to a Django Form class,
     and includes similar validation flags on the various fields, 
     such as required, max_length and default.
-    The field flags can also control how the serializer should be displayed in certain circumstances, such as when rendering to HTML. The {'base_template': 'textarea.html'} flag above is equivalent to using widget=widgets.Textarea on a Django Form class.
-    This is particularly useful for controlling how the browsable API should be displayed.'''
+    The field flags can also control how the serializer should be
+     displayed in certain circumstances, such as when rendering to HTML. 
+    The {'base_template': 'textarea.html'} flag above is equivalent
+     to using widget=widgets.Textarea on a Django Form class.
+    This is particularly useful for controlling how the browsable 
+    API should be displayed.
+    It's important to remember that ModelSerializer classes don't
+    do anything particularly magical, they are simply a shortcut for creating serializer classes:
+    An automatically determined set of fields.
+    Simple default implementations for the create() and update() methods.'''
     
