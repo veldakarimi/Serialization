6b3cfcedcb914704e85aeb93d2888da8edb08b8e
Velda Kiara <veldakarimi@gmail.com>
2020-05-22 12:59:47 +0300

    explanation

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -28,3 +28,11 @@ class SnippetSerializer(serializers.Serializer):
         instance.style = validated_data.get('style', instance.style)
         instance.save()
         return instance
+    '''create() and update() methods define how fully fledged 
+    instances are created or modified when calling serializer.save()
+    A serializer class is very similar to a Django Form class,
+    and includes similar validation flags on the various fields, 
+    such as required, max_length and default.
+    The field flags can also control how the serializer should be displayed in certain circumstances, such as when rendering to HTML. The {'base_template': 'textarea.html'} flag above is equivalent to using widget=widgets.Textarea on a Django Form class.
+    This is particularly useful for controlling how the browsable API should be displayed.'''
+    
