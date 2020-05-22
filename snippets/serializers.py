26f7e81f7ada4c775f0691f6192659117f84a883
Velda Kiara <veldakarimi@gmail.com>
2020-05-22 13:09:38 +0300

    updates

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -7,25 +7,25 @@ class SnippetSerializer(serializers.ModelSerializer):
         model = Snippet
         fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
     
-    def create(self, validated_data):
+def create(self, validated_data):
         #Create and return a new `Snippet` instance, given the validated data.
         
        
-        return Snippet.objects.create(**validated_data)
+    return Snippet.objects.create(**validated_data)
     
     
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
-    '''create() and update() methods define how fully fledged 
+def update(self, instance, validated_data):
+    """
+    Update and return an existing `Snippet` instance, given the validated data.
+    """
+    instance.title = validated_data.get('title', instance.title)
+    instance.code = validated_data.get('code', instance.code)
+    instance.linenos = validated_data.get('linenos', instance.linenos)
+    instance.language = validated_data.get('language', instance.language)
+    instance.style = validated_data.get('style', instance.style)
+    instance.save()
+    return instance
+'''create() and update() methods define how fully fledged 
     instances are created or modified when calling serializer.save()
     A serializer class is very similar to a Django Form class,
     and includes similar validation flags on the various fields, 
